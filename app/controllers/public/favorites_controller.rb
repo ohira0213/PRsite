d04d261e1b5c4a2500ec0b631cad61e86e51125a
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-03-26 10:47:23 +0000

    いいね機能非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,20 +2,20 @@ class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find(params[:post_id])
-    if post
-      favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    if @post
+      favorite = current_user.favorites.new(post_id: @post.id)
       favorite.save
-      redirect_to request.referer
+      #redirect_to request.referer
     end
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    if post
-      favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    if @post
+      favorite = current_user.favorites.find_by(post_id: @post.id)
       favorite.destroy
-      redirect_to request.referer
+      #redirect_to request.referer
     end
   end
 
